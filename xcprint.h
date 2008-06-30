ac65247ec295d4e3b36a72bf3f49377264e7803c
Diego 'Flameeyes' Pettenò <flameeyes@gmail.com>
2008-06-30 20:20:31 +0200

    Update license, move to GPL-3+ rather than GPL-2+, add my copyright.
    Even though I have yet to change all the source file prepare them for
    the changes.
    
    Switch alreayd the license to what I want to release the software with
    (GPL-3).

--- xcprint.h
+++ xcprint.h
@@ -2,20 +2,23 @@
  *  $Id: xcprint.h,v 1.3 2001/10/18 04:49:27 kims Exp $
  * 
  *  xcprint.h - header file for functions in xcprint.c
- *  Copyright (C) 2001 Kim Saunders
+ *  Copyright (c) 2001 Kim Saunders
+ *  Copyright (c) 2008 Diego 'Flameeyes' Pettenò
  *
- *  This program is free software; you can redistribute it and/or modify
+ *  This file is part of xcbclip.
+ *
+ *  xcbclip is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  xcbclip is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
+ *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  along with xcbclip.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* functions in xcprint.c */
